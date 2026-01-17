1fb9ccf8558baae8213f2041b1dc92ee3b70a573
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-01-17 19:54:09 +0530

    Update BooksList.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,50 +1,57 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchBooks, setSort } from "../redux/actions";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooks, setSortCriteria, setSortOrder } from '../redux/actions';
 
 const BooksList = () => {
   const dispatch = useDispatch();
-  const { books, loading, error, sortBy, order } = useSelector(
-    (state) => state
-  );
+  const { books, loading, error, sortBy, sortOrder } = useSelector((state) => state);
 
   useEffect(() => {
     dispatch(fetchBooks());
   }, [dispatch]);
 
+  // Sorting Logic
   const sortedBooks = [...books].sort((a, b) => {
-    const aVal = a[sortBy].toLowerCase();
-    const bVal = b[sortBy].toLowerCase();
+    // Access properties dynamically based on sortBy (title, author, publisher)
+    // We map UI values to API keys if necessary. Assuming API keys are 'title', 'author', 'publisher'
+    const valA = (a[sortBy] || '').toString().toLowerCase();
+    const valB = (b[sortBy] || '').toString().toLowerCase();
 
-    if (aVal < bVal) return order === "asc" ? -1 : 1;
-    if (aVal > bVal) return order === "asc" ? 1 : -1;
+    if (valA < valB) return sortOrder === 'asc' ? -1 : 1;
+    if (valA > valB) return sortOrder === 'asc' ? 1 : -1;
     return 0;
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  if (loading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="error">Error: {error}</div>;
 
   return (
-    <div>
-      {/* SORT DROPDOWNS */}
-      <select
-        value={sortBy}
-        onChange={(e) => dispatch(setSort(e.target.value, order))}
-      >
-        <option value="title">Title</option>
-        <option value="author">Author</option>
-        <option value="publisher">Publisher</option>
-      </select>
+    <div className="books-container">
+      <h1>Book List</h1>
+      
+      {/* Controls Container: Strict structure for Selectors */}
+      <div className="controls">
+        {/* select:nth-child(1) -> Sort Criteria */}
+        <select 
+          value={sortBy} 
+          onChange={(e) => dispatch(setSortCriteria(e.target.value))}
+        >
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="publisher">Publisher</option>
+        </select>
 
-      <select
-        value={order}
-        onChange={(e) => dispatch(setSort(sortBy, e.target.value))}
-      >
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-      </select>
+        {/* select:nth-child(2) -> Sort Order */}
+        <select 
+          value={sortOrder} 
+          onChange={(e) => dispatch(setSortOrder(e.target.value))}
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
 
-      {/* BOOKS TABLE */}
+      {/* Table Display */}
       <table>
         <thead>
           <tr>
@@ -55,8 +62,9 @@ const BooksList = () => {
           </tr>
         </thead>
         <tbody>
-          {sortedBooks.map((book) => (
-            <tr key={book.primary_isbn13}>
+          {sortedBooks.map((book, index) => (
+            // Use ISBN as key if available, otherwise index
+            <tr key={book.primary_isbn13 || index}>
               <td>{book.title}</td>
               <td>{book.author}</td>
               <td>{book.publisher}</td>
