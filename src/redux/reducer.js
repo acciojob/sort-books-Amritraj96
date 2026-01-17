2722f20d62bb6409ca9c87a9b72ab401e7f8999a
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-01-17 19:52:56 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,36 +2,33 @@ import {
   FETCH_BOOKS_REQUEST,
   FETCH_BOOKS_SUCCESS,
   FETCH_BOOKS_FAILURE,
-  SET_SORT,
-} from "./actions";
+  SET_SORT_CRITERIA,
+  SET_SORT_ORDER,
+} from './actions';
 
 const initialState = {
   books: [],
   loading: false,
   error: null,
-  sortBy: "title",
-  order: "asc",
+  sortBy: 'title', // default: Title
+  sortOrder: 'asc', // default: Ascending
 };
 
-export const booksReducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKS_REQUEST:
       return { ...state, loading: true };
-
     case FETCH_BOOKS_SUCCESS:
-      return { ...state, loading: false, books: action.payload };
-
+      return { ...state, loading: false, books: action.payload, error: null };
     case FETCH_BOOKS_FAILURE:
       return { ...state, loading: false, error: action.payload };
-
-    case SET_SORT:
-      return {
-        ...state,
-        sortBy: action.payload.sortBy,
-        order: action.payload.order,
-      };
-
+    case SET_SORT_CRITERIA:
+      return { ...state, sortBy: action.payload };
+    case SET_SORT_ORDER:
+      return { ...state, sortOrder: action.payload };
     default:
       return state;
   }
 };
+
+export default reducer;
